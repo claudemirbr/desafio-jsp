436ba61cb44b583ea9f064dd3340d35976b77bf5
claudemir.pinto <claudemir.pinto@softplan.com.br>
2021-09-26 10:19:02 -0300

    Adiciona sql dialect

--- src/main/java/com/br/atomicweb/model/Contato.java
+++ src/main/java/com/br/atomicweb/model/Contato.java
@@ -9,29 +9,34 @@ import org.hibernate.annotations.GenericGenerator;
 
 @Entity
 public class Contato {
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@GenericGenerator(name = "increment", strategy = "increment")
 	private Long id;
 	private String nome;
 	private String email;
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public String getEmail() {
 		return email;
 	}
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
