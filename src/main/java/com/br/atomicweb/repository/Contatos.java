f61e6ca40a0bafabf6581d2086029287eab138e2
claudemir.pinto <claudemir.pinto@softplan.com.br>
2021-09-27 13:43:41 -0300

    Adiciona parametros de conexao heroku

--- src/main/java/com/br/atomicweb/repository/Contatos.java
+++ src/main/java/com/br/atomicweb/repository/Contatos.java
@@ -13,27 +13,25 @@ import com.br.atomicweb.model.Contato;
 //https://gist.github.com/mlecoutre/4088178
 
 public class Contatos {
-	private static final String PERSISTENCE_UNIT_NAME = "default";
-	private static EntityManagerFactory factory;
+	//private static final String PERSISTENCE_UNIT_NAME = "default";
+	//private static EntityManagerFactory factory;
 
-	public Contatos() {
-		factory = Persistence.createEntityManagerFactory(PERSISTENCE_UNIT_NAME);
-	}
+
 
 	public List<Contato> getContatos() throws ClassNotFoundException {
-		EntityManager em = factory.createEntityManager();
+		EntityManager em = CriaEntityManagerFactory.getEntityManagerFactory().createEntityManager();
 		return em.createQuery("select c from Contato c", Contato.class).getResultList();
 	}
 
 	public Contato getContato(Contato contato) {
-		EntityManager em = factory.createEntityManager();
+		EntityManager em = CriaEntityManagerFactory.getEntityManagerFactory().createEntityManager();
 		return (Contato) em.createQuery("select c from Contato c where id = :id")
 			.setParameter("id", contato.getId())
 			.getSingleResult();
 	}
 
 	public void salvaContato(Contato contato) {
-		EntityManager em = factory.createEntityManager();
+		EntityManager em = CriaEntityManagerFactory.getEntityManagerFactory().createEntityManager();
 		em.getTransaction().begin();
 		em.persist(contato);
 		em.getTransaction().commit();
@@ -41,7 +39,7 @@ public class Contatos {
 	}
 
 	public void editaContato(Contato contato) {
-		EntityManager em = factory.createEntityManager();
+		EntityManager em = CriaEntityManagerFactory.getEntityManagerFactory().createEntityManager();
 		em.getTransaction().begin();
 		em.merge(contato);
 		em.flush();
@@ -50,7 +48,7 @@ public class Contatos {
 	}
 
 	public void deletaContato(Contato contato) {
-		EntityManager em = factory.createEntityManager();
+		EntityManager em = CriaEntityManagerFactory.getEntityManagerFactory().createEntityManager();
 		em.getTransaction().begin();
 		em.remove(em.contains(contato) ? contato : em.merge(contato));
 		em.getTransaction().commit();
